1b6901f238b2b023e0d393507aaeec5d8af47261
Karthik Ravivarapu <22068548+ravivarapuk@users.noreply.github.com>
2020-01-29 09:43:43 +0530

    adding code for spotify automation

--- spotify_automation/mute_at_ad.py
+++ spotify_automation/mute_at_ad.py
@@ -1,2 +1,43 @@
-import requests
-import os
+import time
+import pyautogui
+
+
+def _openSpotify():
+    pyautogui.hotkey('command', 'space')
+    pyautogui.typewrite('spotify app')
+    time.sleep(1)
+    pyautogui.press('enter')
+
+
+def _locateIcon(icon):
+    try:
+        img = pyautogui.locateCenterOnScreen(f'{icon}')
+        return img
+    except TypeError:
+        pass
+        # ignores error and continues code - needed because
+        # locateCenterOnScreen() does not return None in case of match failure
+
+
+def _clickSpeaker(sX, sY):
+    pyautogui.click(x=sX, y=sY)
+
+
+# Driver Code
+_openSpotify()
+
+playTime = locateIcon('ad.png')
+if playTime is None:
+    playTime = locateIcon('ad2.png')
+if playTime is not None:
+    try:
+        sX, sY = locateIcon('speakerBtn.png')
+        clickSpeaker(sX, sY)
+        time.sleep(30)
+        openSpotify()
+        clickSpeaker(sX, sY)
+
+    except TypeError:
+        print('Speaker button out of range')
+else:
+    print('No ads are playing')
